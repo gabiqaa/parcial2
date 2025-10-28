b723429ef3cb2ec6bbc77f190bc8da23a0c71f15
DavidY3Wv <a2024110134@estudiantes.upsa.edu.bo>
2025-10-27 21:50:00 -0400

    Corregir

--- Eparcial2/Controllers/AuthController.cs
+++ Eparcial2/Controllers/AuthController.cs
@@ -4,8 +4,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Eparcial2.Models;
 using Eparcial2.Data;
+using Eparcial2.Models;
 
 namespace parcial2.Controllers
 {
